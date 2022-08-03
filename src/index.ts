f80cb5f5b97a69d8a6d396fdbac2c6c9152abcf2
Keri Warr <keri@warr.ca>
2022-08-02 21:23:25 -0700

    The get endpoints

--- src/index.ts
+++ src/index.ts
@@ -12,21 +12,38 @@ export class ManifoldError extends Error {
   }
 }
 
+const log = (message: string) => {
+  if (process.env.NODE_ENV === "production") return;
+
+  console.log(message);
+};
+
+const logError = (message: Error) => {
+  if (process.env.NODE_ENV === "production") return;
+
+  console.error(message);
+};
+
 const wrappedFetch = async <RetVal>(...args: Parameters<typeof fetch>) => {
+  log(`fetching '${args[0]}'...`);
   const resp = await fetch(...args);
 
   const contentType = resp.headers.get("content-type");
 
   if (!contentType || contentType.indexOf("application/json") === -1) {
-    throw new Error(
+    const error = new Error(
       `Unexpectedly received non-JSON response: ${await resp.text()}`
     );
+    logError(error);
+    throw error;
   }
 
   const json = await resp.json();
 
   if (!resp.ok) {
-    throw new ManifoldError(resp.status, json);
+    const error = new ManifoldError(resp.status, json);
+    logError(error);
+    throw error;
   }
 
   return json as RetVal;
@@ -44,17 +61,17 @@ interface GenericCreateMarketArgs {
 }
 
 interface BinaryCreateMarketArgs extends GenericCreateMarketArgs {
-  type: "BINARY";
+  outcomeType: "BINARY";
   // between 1 and 99 inclusive
   initialProb: number;
 }
 
 interface FreeResponseCreateMarketArgs extends GenericCreateMarketArgs {
-  type: "FREE_RESPONSE";
+  outcomeType: "FREE_RESPONSE";
 }
 
 interface NumericCreateMarketArgs extends GenericCreateMarketArgs {
-  type: "NUMERIC";
+  outcomeType: "NUMERIC";
   // The minimum value that the market may resolve to.
   min: number;
   // The maximum value that the market may resolve to.
@@ -66,23 +83,334 @@ type CreateMarketArgs =
   | FreeResponseCreateMarketArgs
   | NumericCreateMarketArgs;
 
-interface CreateMarketResponse {
+// TODO: get build to output comments?
+interface User {
+  id: string; // user's unique id
+  createdTime: number;
+
+  name: string; // display name, may contain spaces
+  username: string; // username, used in urls
+  // keri: wasn't present in dev. making partial
+  url?: string; // link to user's profile
+  avatarUrl?: string;
+
+  bio?: string;
+  bannerUrl?: string;
+  website?: string;
+  twitterHandle?: string;
+  discordHandle?: string;
+
+  // Note: the following are here for convenience only and may be removed in the future.
+  balance: number;
+  totalDeposits: number;
+  // keri: wasn't present in dev. making partial
+  totalPnLCached?: number;
+
+  // maybe guaranteed? no idea
+  creatorVolumeCached?: {
+    allTime: number;
+    weekly: number;
+    daily: number;
+    monthly: number;
+  };
+  profitCached?: {
+    allTime: number;
+    weekly: number;
+    daily: number;
+    monthly: number;
+  };
+
+  // only for /me
+  followedCategories?: string[];
+  followerCountCached?: number;
+}
+
+interface Bet {
   id: string;
+  contractId: string;
+
+  amount: number; // bet size; negative if SELL bet
+  loanAmount?: number;
+  outcome: string;
+  shares: number; // dynamic parimutuel pool weight; negative if SELL bet
+
+  probBefore: number;
+  probAfter: number;
+
+  sale?: {
+    amount: number; // amount user makes from sale
+    betId: string; // id of bet being sold
+  };
+
+  fees?: {
+    liquidityFee: number;
+    creatorFee: number;
+    platformFee: number;
+  };
+
+  isSold?: boolean; // true if this BUY bet has been sold
+  isAnte?: boolean;
+
+  createdTime: number;
+}
+
+interface Comment {
+  userName: string;
+  userUsername: string;
+  id: string;
+  userAvatarUrl: string;
+  text: string;
+  createdTime: number;
+  userId: string;
+  contractId: string;
+}
+
+interface Answer {
+  createdTime: number;
+  avatarUrl: string;
+  id: string;
+  username: string;
+  number: number;
+  name: string;
+  contractId: string;
+  text: string;
+  userId: string;
+  probability: number;
+}
+
+interface FullMarket extends LiteMarket {
+  bets: Bet[];
+  comments: Comment[];
+  answers?: Answer[];
+}
+interface LiteMarket {
+  // Unique identifer for this market
+  id: string;
+
+  // Attributes about the creator
+  creatorUsername: string;
+  creatorName: string;
+  createdTime: number; // milliseconds since epoch
+  creatorAvatarUrl?: string;
+
+  // Market attributes. All times are in milliseconds since epoch
+  closeTime?: number; // Min of creator's chosen date, and resolutionTime
+  question: string;
+  description: unknown; // FIXME: ...
+
+  // A list of tags on each market. Any user can add tags to any market.
+  // This list also includes the predefined categories shown as filters on the home page.
+  tags: string[];
+
+  // Note: This url always points to https://manifold.markets, regardless of what instance the api is running on.
+  // This url includes the creator's username, but this doesn't need to be correct when constructing valid URLs.
+  //   i.e. https://manifold.markets/Austin/test-market is the same as https://manifold.markets/foo/test-market
+  url: string;
+
+  outcomeType: CreateMarketArgs["outcomeType"]; // BINARY, FREE_RESPONSE, or NUMERIC
+  mechanism: "dpm-2" | "cpmm-1";
+
+  probability: number;
+  // keri: might be nice to discriminate the type by outcometype. e.g. the only outcomes in the pool are "YES" and "NO" for binary
+  pool: Record<string, number>; // For CPMM markets, the number of shares in the liquidity pool. For DPM markets, the amount of mana invested in each answer.
+  p?: number; // CPMM markets only, probability constant in y^p * n^(1-p) = k
+  totalLiquidity?: number; // CPMM markets only, the amount of mana deposited into the liquidity pool
+
+  volume: number;
+  volume7Days: number;
+  volume24Hours: number;
+
+  isResolved: boolean;
+  resolutionTime?: number;
+  resolution?: string;
+  resolutionProbability?: number; // Used for BINARY markets resolved to MKT
+}
+
+interface CreateMarketResponse extends Omit<LiteMarket, "url" | "probability"> {
+  slug: string; // keri: don't know for sure this is guaranteed to be present but I sure hope it is...
+
+  creatorId?: string;
+
+  lowercaseTags?: string[];
+
+  initialProbability?: number;
+
+  visibility?: string;
+  collectedFees?: {
+    creatorFee: number;
+    liquidityFee: number;
+    platformFee: number;
+  };
+}
+
+interface Group {
+  id: string;
+  name: string;
+  about?: string;
+  createdTime: number;
+  chatDisabled: boolean;
+  contractIds: string[];
+  creatorId: string;
+  memberIds: string[];
+  mostRecentActivityTime: number;
+  slug: string;
+  anyoneCanJoin: boolean;
+  type: string;
 }
 
 export class Manifold {
-  constructor(public apiKey: string) {}
+  constructor(public apiKey?: string) {}
 
-  public post<RetVal, Body>({ path, body }: { path: string; body: Body }) {
+  public get<
+    RetVal,
+    Params extends Record<string, string> | undefined = undefined
+  >({
+    path,
+    params,
+    requiresAuth = false,
+    apiKey = this.apiKey,
+  }: {
+    path: string;
+    params?: Params;
+    requiresAuth?: boolean;
+    apiKey?: string;
+  }) {
+    if (requiresAuth && !apiKey) {
+      throw new Error("Missing API Key");
+    }
+
+    const fullpath = (() => {
+      const pathname = `${BASE_URL}/api/v0${path}`;
+
+      if (!params) return pathname;
+
+      return `${pathname}?${new URLSearchParams(params).toString()}`;
+    })();
+
+    return wrappedFetch<RetVal>(fullpath, {
+      method: "GET",
+
+      headers: {
+        ...(requiresAuth && {
+          Authorization: `Key ${apiKey}`,
+        }),
+      },
+    });
+  }
+
+  public post<RetVal, Body>({
+    path,
+    body,
+    requiresAuth = true,
+    apiKey = this.apiKey,
+  }: {
+    path: string;
+    body: Body;
+    requiresAuth?: boolean;
+    apiKey?: string;
+  }) {
+    if (requiresAuth && !apiKey) {
+      throw new Error("Missing API Key");
+    }
+
     return wrappedFetch<RetVal>(`${BASE_URL}/api/v0${path}`, {
       method: "POST",
 
       headers: {
-        Authorization: `Key ${this.apiKey}`,
+        ...(requiresAuth && {
+          Authorization: `Key ${apiKey}`,
+        }),
         "Content-Type": "application/json",
       },
 
       body: JSON.stringify(body),
+    });
+  }
+
+  public getMe() {
+    return this.get<User>({ path: "/me", requiresAuth: true });
+  }
+
+  public getUser({
+    username,
+    id,
+  }: { username: string; id?: never } | { username?: never; id: string }) {
+    if (username) {
+      return this.get<User>({ path: `/user/${username}` });
+    } else if (id) {
+      return this.get<User>({ path: `/user/by-id/${id}` });
+    } else {
+      throw new Error("Need username or id to fetch user");
+    }
+  }
+
+  public getUsers() {
+    return this.get<User[]>({ path: "/users" });
+  }
+
+  public getGroups() {
+    return this.get<Group[]>({ path: "/groups" });
+  }
+
+  public getGroup({
+    slug,
+    id,
+  }: { slug: string; id?: never } | { slug?: never; id: string }) {
+    if (slug) {
+      return this.get<Group>({ path: `/groups/${slug}` });
+    } else if (id) {
+      return this.get<Group>({ path: `/groups/by-id/${id}` });
+    } else {
+      throw new Error("Need slug or id to fetch group");
+    }
+  }
+
+  public getMarket({
+    slug,
+    id,
+  }: { slug: string; id?: never } | { slug?: never; id: string }) {
+    if (slug) {
+      return this.get<FullMarket>({ path: `/slug/${slug}` });
+    } else if (id) {
+      return this.get<FullMarket>({ path: `/market/${id}` });
+    } else {
+      throw new Error("Need slug or id to fetch market");
+    }
+  }
+
+  public getMarkets({ limit, before }: { limit?: number; before?: string }) {
+    const params = {
+      ...(limit && { limit: limit.toString() }),
+      ...(before && { before }),
+    };
+
+    return this.get<LiteMarket[], typeof params>({
+      path: "/markets",
+      params,
+    });
+  }
+
+  public getBets({
+    username,
+    market,
+    limit,
+    before,
+  }: {
+    username?: string;
+    market?: string;
+    limit?: number;
+    before?: string;
+  }) {
+    const params = {
+      ...(username && { username }),
+      ...(market && { market }),
+      ...(limit && { limit: limit.toString() }),
+      ...(before && { before }),
+    };
+
+    return this.get<Bet[], typeof params>({
+      path: "/bets",
+      params,
     });
   }
 
