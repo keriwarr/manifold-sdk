c08603db6ac3b682cdcd4aae7eb903ab509e2e4a
Keri Warr <keri@warr.ca>
2022-08-02 19:22:33 -0700

    linting and such

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,63 @@
 import fetch from "node-fetch";
 
-console.log("test");
+const BASE_URL = (() => {
+  if (process.env.NODE_ENV === "production") return "https://manifold.markets";
+  return "https://dev.manifold.markets";
+})();
 
-export const foo = (...args: Parameters<typeof fetch>) => fetch(...args);
+console.log();
+export class Manifold {
+  constructor(public apiKey: string) {}
+
+  public post<RetVal>({ path, body }: { path: string; body: any }) {
+    return fetch(`${BASE_URL}/api/v0${path}`, {
+      method: "POST",
+      headers: {
+        Authorization: `Key ${this.apiKey}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+  }
+
+  // public createMarket: ({
+  //     apiKey,
+  //     summary,
+  //     terms,
+  //     maturesAt,
+  //     probability,
+  //     wagerId,
+  //   }: {
+  //     apiKey: string;
+  //     summary: string;
+  //     terms: string;
+  //     maturesAt: number;
+  //     probability: number;
+  //     wagerId: string;
+  //   }) =>
+  //     postManifoldMarkets({
+  //       path: "/market",
+  //       apiKey,
+  //       body: {
+  //         outcomeType: "BINARY",
+  //         question: summary,
+  //         description: {
+  //           type: "doc",
+  //           content: [
+  //             {
+  //               type: "paragraph",
+  //               content: [
+  //                 {
+  //                   type: "text",
+  //                   text: `${terms}\n\nThis market is derived from a wager on WagerWith.me: ${process.env.BASE_URL}/wager/${wagerId}`,
+  //                 },
+  //               ],
+  //             },
+  //           ],
+  //         },
+  //         closeTime: maturesAt, // FIXME: what if today...
+  //         tags: ["wagerwithme"],
+  //         initialProb: probability * 100, // me gusta
+  //       },
+  //     }),
+}
